8b8622bec95829fc67abc6daa84a5befac3f9ed3
jinwanlin123@gmail.com <jinwanlin123@gmail.com>
2013-11-18 09:42:43 +0800

    s

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foods::Application.config.secret_token = '717197f56044ccbc7d7c55a38d2e87d9363625cf5c0013a70e904d45d3972de9ed4ceb9946452b9236343ee7f6ec781e04f20a378631bde5ccc3e34f874ebd9b'
